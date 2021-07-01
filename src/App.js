52de84bdc9e446c433f7aaaef6439373991f6f97
cesare.soldini <cesare.soldini@accurat.it>
2021-07-01 17:24:54 +0200

    🚜 Refactor into Scatterplot component

--- src/App.js
+++ src/App.js
@@ -1,84 +1,9 @@
-import { useState, useEffect } from 'react'
-import * as d3 from 'd3'
-import { uniq, zipObject } from 'lodash'
+import { Scatterplot } from './Scatterplot'
 
-const URL = 'https://raw.githubusercontent.com/vega/vega-datasets/master/data/penguins.json'
-
-function App() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [dataset, setDataset] = useState([])
-
-  useEffect(() => {
-    if (dataset.length === 0) return
-
-    console.table(dataset.slice(0, 10))
-    console.log(d3.extent(dataset.map((d) => d['Beak Length (mm)'])))
-  }, [dataset])
-
-  useEffect(() => {
-    setIsLoading(true)
-    fetch(URL)
-      .then((response) => response.json())
-      .then((json) => {
-        setDataset(json)
-      })
-      .catch(() => {
-        window.alert('Errore!')
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
-  }, [])
-
-  const margins = {
-    top: 10,
-    left: 10,
-    right: 10,
-    bottom: 10,
-  }
-
-  const xScale = d3
-    .scaleLinear()
-    .range([0 + margins.left, 500 - margins.right])
-    .domain(d3.extent(dataset.map((d) => d['Beak Length (mm)'])))
-
-  const yScale = d3
-    .scaleLinear()
-    .range([500 - margins.bottom, 0 + margins.top])
-    .domain(d3.extent(dataset.map((d) => d['Flipper Length (mm)'])))
-
-  const species = uniq(dataset.map((d) => d['Species']))
-  const colors = ['tomato', 'steelblue', 'green']
-  const speciesColorsTranslation = zipObject(species, colors)
-
-  const colorScale = (specie) => {
-    return speciesColorsTranslation[specie]
-  }
-
+export function App() {
   return (
     <div>
-      {isLoading ? (
-        <div>L O A D I N G . . .</div>
-      ) : (
-        <svg width={500} height={500}>
-          <rect fill="aliceblue" width={500} height={500} />
-
-          {dataset
-            .filter((d) => d['Beak Length (mm)'] !== null && d['Flipper Length (mm)'] !== null)
-            .map((datum, i) => (
-              <g key={i}>
-                <circle
-                  r={5}
-                  cx={xScale(datum['Beak Length (mm)'])}
-                  cy={yScale(datum['Flipper Length (mm)'])}
-                  fill={colorScale(datum['Species'])}
-                />
-              </g>
-            ))}
-        </svg>
-      )}
+      <Scatterplot />
     </div>
   )
 }
-
-export default App
