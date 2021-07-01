be1972bac1d11f436742cd7dd63c5e051b90db35
cesare.soldini <cesare.soldini@accurat.it>
2021-07-01 17:08:11 +0200

    💅 Prettier

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
     if (dataset.length === 0) return
 
     console.table(dataset.slice(0, 10))
-    console.log(d3.extent(dataset.map(d => d['Beak Length (mm)'])))
+    console.log(d3.extent(dataset.map((d) => d['Beak Length (mm)'])))
   }, [dataset])
 
   useEffect(() => {
     setIsLoading(true)
     fetch(URL)
       .then((response) => response.json())
-      .then(json => {
+      .then((json) => {
         setDataset(json)
       })
       .catch(() => {
@@ -37,15 +37,17 @@ function App() {
     bottom: 10,
   }
 
-  const xScale = d3.scaleLinear()
+  const xScale = d3
+    .scaleLinear()
     .range([0 + margins.left, 500 - margins.right])
-    .domain(d3.extent(dataset.map(d => d['Beak Length (mm)'])))
+    .domain(d3.extent(dataset.map((d) => d['Beak Length (mm)'])))
 
-  const yScale = d3.scaleLinear()
+  const yScale = d3
+    .scaleLinear()
     .range([500 - margins.bottom, 0 + margins.top])
-    .domain(d3.extent(dataset.map(d => d['Flipper Length (mm)'])))
+    .domain(d3.extent(dataset.map((d) => d['Flipper Length (mm)'])))
 
-  const species = uniq(dataset.map(d => d['Species']))
+  const species = uniq(dataset.map((d) => d['Species']))
   const colors = ['tomato', 'steelblue', 'green']
   const speciesColorsTranslation = zipObject(species, colors)
 
@@ -55,18 +57,15 @@ function App() {
 
   return (
     <div>
-      {isLoading
-      ? (<div>L O A D I N G . . .</div>)
-      : (
+      {isLoading ? (
+        <div>L O A D I N G . . .</div>
+      ) : (
         <svg width={500} height={500}>
           <rect fill="aliceblue" width={500} height={500} />
 
           {dataset
-            .filter(d =>
-              d['Beak Length (mm)'] !== null &&
-              d['Flipper Length (mm)'] !== null
-            )
-            .map((datum, i) =>
+            .filter((d) => d['Beak Length (mm)'] !== null && d['Flipper Length (mm)'] !== null)
+            .map((datum, i) => (
               <g key={i}>
                 <circle
                   r={5}
@@ -75,12 +74,11 @@ function App() {
                   fill={colorScale(datum['Species'])}
                 />
               </g>
-            )
-          }
+            ))}
         </svg>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
