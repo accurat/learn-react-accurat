37ee5b45614b2045e8ee4119ebd7382d020cc3fb
cesare.soldini <cesare.soldini@accurat.it>
2021-07-01 15:24:19 +0200

    👽 Initialize API call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { useState, useEffect } from 'react'
+
+const URL = 'https://raw.githubusercontent.com/vega/vega-datasets/master/data/penguins.json'
+
 function App() {
-  return <div>ciao</div>
+  const [isLoading, setIsLoading] = useState(false)
+  const [dataset, setDataset] = useState(null)
+
+  console.log(dataset, isLoading)
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(URL)
+      .then((response) => response.json())
+      .then(json => {
+        setDataset(json)
+      })
+      .catch(() => {
+        window.alert('Errore!')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [])
+
+  return (
+    <div>
+      <svg>
+
+      </svg>
+    </div>
+  );
 }
 
 export default App;
